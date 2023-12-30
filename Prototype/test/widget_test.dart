d6a494577f1138efff441ed0fed2ac68b80681bf
Leon <looyang01@gmail.com>
2023-12-30 22:41:21 +0800

    Added Class Diagram

--- Prototype/test/widget_test.dart
+++ Prototype/test/widget_test.dart
@@ -7,13 +7,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:prototype/app/mainview.dart';
 
 import 'package:prototype/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MainView());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
