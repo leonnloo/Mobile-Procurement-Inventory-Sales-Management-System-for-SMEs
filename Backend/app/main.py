6e78d9fcbc2360cb4bb5f98391b99d31f94d412d
Leon <looyang01@gmail.com>
2024-02-27 23:15:32 +0800

    Added getter request for each model

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI, Body, Request
 from routes.user import user_router
 from routes.auth import auth_router
 from routes.forms import form_router
+from routes.get import get_router
 
 app = FastAPI()
 
 app.include_router(user_router)
 app.include_router(auth_router)
 app.include_router(form_router)
+app.include_router(get_router)
