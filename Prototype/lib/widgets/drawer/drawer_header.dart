d0382ca17e36553d348e75a5f6f94383a4377db0
Leon <looyang01@gmail.com>
2024-03-04 15:27:37 +0800

    Linked up drawer header data

--- Prototype/lib/widgets/drawer/drawer_header.dart
+++ Prototype/lib/widgets/drawer/drawer_header.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:prototype/app/user_profile.dart';
+import 'package:prototype/util/user_controller.dart';
 
 class HeaderDrawer extends StatefulWidget {
   const HeaderDrawer({super.key});
@@ -12,43 +15,102 @@ class HeaderDrawer extends StatefulWidget {
 class HeaderDrawerState extends State<HeaderDrawer> {
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        Get.to(() => const UserProfileScreen());
-      },
-      child: Container(
-        color: Colors.red[400],
-        width: double.infinity,
-        height: 220,
-        padding: const EdgeInsets.only(top: 20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              margin: const EdgeInsets.only(bottom: 10),
-              height: 70,
-              decoration: const BoxDecoration(
-                shape: BoxShape.circle,
-                image: DecorationImage(
-                  image: AssetImage('images/nottingham.jpg'),
+    return FutureBuilder(
+      future: _fetchUserData(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const CircularProgressIndicator(); // or a loading indicator
+        } else if (snapshot.hasError) {
+          print('object');
+          return Text('Error: ${snapshot.error}');
+        } else {
+          final user = snapshot.data;
+          if (user != null) {
+            return GestureDetector(
+              onTap: () {
+                Get.to(() => const UserProfileScreen());
+              },
+              child: Container(
+                color: Colors.red[400],
+                width: double.infinity,
+                height: 220,
+                padding: const EdgeInsets.only(top: 20.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(bottom: 10),
+                      height: 70,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                          image: AssetImage('images/nottingham.jpg'),
+                        ),
+                      ),
+                    ),
+                    // TO DO: user information
+                    Text(           
+                      user['employee_name'],
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                    Text(
+                      user["email"],
+                      style: TextStyle(
+                        color: Colors.grey[200],
+                        fontSize: 14,
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ),
-            // TO DO: user information
-            const Text(           
-              "Notts",
-              style: TextStyle(color: Colors.white, fontSize: 20),
-            ),
-            Text(
-              "notts@nottingham.edu.cn",
-              style: TextStyle(
-                color: Colors.grey[200],
-                fontSize: 14,
-              ),
-            ),
-          ],
-        ),
-      ),
+            );
+          }
+          else {
+            return Container(
+                color: Colors.red[400],
+                width: double.infinity,
+                height: 220,
+                padding: const EdgeInsets.only(top: 20.0),
+                child: const Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // TO DO: user information
+                    Text(           
+                      "Unable to load user data",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                  ],
+                ),
+              );
+            }
+        }
+      }
     );
+  }
+
+  Future<Map<String, dynamic>> _fetchUserData() async {
+    final userController = Get.put(UserLoggedInController());
+    try {
+      print(userController.currentUser.value);
+      final user = await requestUtil.getUser(userController.currentUser.value);
+      return jsonDecode(user.body);
+      // final tokenResponse = await requestUtil.getToken(userController.currentUser.value);
+
+      // if (tokenResponse == null || tokenResponse.body == null) {
+      //   throw Exception('Token response is null or missing body.');
+      // }
+
+      // final tokenData = jsonDecode(tokenResponse.body);
+      // if (tokenData == null || tokenData['access_token'] == null) {
+      //   throw Exception('Access token is null or missing.');
+      // }
+
+      // final user = requestUtil.getUser(tokenData['access_token']);
+
+      // return {'token': tokenData['access_token'], 'user': user};
+    } catch (error) {
+      print('Error in _fetchUserData: $error');
+      rethrow; // Rethrow the error to be caught by FutureBuilder
+    }
   }
 }
