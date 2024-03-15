ef75e5cb31be4374c949042bc9376fcee4b43f8f
Leon <looyang01@gmail.com>
2024-03-15 23:03:23 +0800

    Saved for llm experiment

--- Backend/app/routes/chatbot/entities_extract.py
+++ Backend/app/routes/chatbot/entities_extract.py
@@ -10,17 +10,25 @@ def extract_entities(text):
     for ent in doc.ents:
         if ent.label_ == "PERSON":  # Extract person names
             entities["name"] = ent.text
+        # elif ent.label_ == "ID":
+        #     entities["ID"] = ent.text
         elif ent.label_ == "EMAIL":  # Extract email addresses
             entities["email"] = ent.text
         elif ent.label_ == "ORG":  # Extract organization or company names
-            entities["organization"] = ent.text
+            entities["business_name"] = ent.text
         elif ent.label_ == "PHONE":  # Extract phone numbers
-            entities["phone"] = ent.text
+            entities["phone_number"] = ent.text
         elif ent.label_ == "DATE":  # Extract dates
             entities["date"] = ent.text
         elif ent.label_ == "MONEY":  # Extract monetary values
             entities["money"] = ent.text
+        elif ent.label_ == "INVENTORY":  
+            entities["product"] = ent.text
         elif ent.label_ == "PRODUCT":  # Extract product names
             entities["product"] = ent.text
+        # elif ent.label_ == "ADDRESS":  # Extract product names
+        #     entities["product"] = ent.text
+        # elif ent.label_ == "CRITICAL_LEVEL":  
+        #     entities["product"] = ent.text
             
     return entities
