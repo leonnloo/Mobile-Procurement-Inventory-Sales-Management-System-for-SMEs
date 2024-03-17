c6b0119b62f789f4a2b64ccd42b5e21c572408af
leonnloo <looyang01@gmail.com>
2024-03-17 23:44:37 +0800

    Modified quirks

--- Backend/app/routes/chatbot/spacyllm.py
+++ Backend/app/routes/chatbot/spacyllm.py
@@ -1,5 +1,5 @@
 from spacy_llm.util import assemble
 
-nlp = assemble("config.cfg")
+nlp = assemble("routes/chatbot/config.cfg")
 doc = nlp("Jack and Jill rode up the hill in Les Deux Alpes")
 print([(ent.text, ent.label_) for ent in doc.ents])
