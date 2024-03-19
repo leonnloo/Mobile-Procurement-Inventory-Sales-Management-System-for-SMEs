c44060c69a1009a4e502db60776d480d6e549492
leonnloo <looyang01@gmail.com>
2024-03-20 00:33:40 +0800

    Linked sales target

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -8,6 +8,7 @@ from routes.notes import note_router
 from routes.delete import delete_router 
 from routes.verification import verify_router 
 from routes.chatbot.chatbot import chatbot_router
+from routes.sales_management import sales_management_router
 from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
@@ -27,3 +28,4 @@ app.include_router(note_router)
 app.include_router(delete_router)
 app.include_router(verify_router)
 app.include_router(chatbot_router)
+app.include_router(sales_management_router)
