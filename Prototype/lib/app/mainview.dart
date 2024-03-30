e91cdafa1ce1cc76a343440d8fc12324c92fdde2
leonnloo <looyang01@gmail.com>
2024-03-30 22:46:40 +0800

    Added loading start up

--- Prototype/lib/app/mainview.dart
+++ Prototype/lib/app/mainview.dart
@@ -10,34 +10,54 @@ class MainView extends StatelessWidget {
   const MainView({super.key});
 
   @override
-Widget build(BuildContext context) {
-  return GetMaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: 'GRP-Team 14',
-    themeMode: ThemeMode.light,
-    theme: ThemeData(
-      fontFamily: GoogleFonts.lato().fontFamily,
-      colorScheme: lightColorScheme,
-      useMaterial3: true,
-    ),
-    transitionDuration: const Duration(milliseconds: 500),
-    home: FutureBuilder<bool>(
-      future: UserSession.isUserLoggedIn(), // Assuming this is your method to check login status
-      builder: (context, snapshot) {
-        // Check if the future has completed
-        if (snapshot.connectionState == ConnectionState.done) {
-          // If we can determine the login status
-          if (snapshot.hasData && snapshot.data == true) {
-            return const HomeScreen(); // Or wherever you want to route after login
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'GRP-Team 14',
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        fontFamily: GoogleFonts.lato().fontFamily,
+        colorScheme: lightColorScheme,
+        useMaterial3: true,
+      ),
+      transitionDuration: const Duration(milliseconds: 500),
+      home: FutureBuilder<bool>(
+        future: UserSession.isUserLoggedIn(), // Assuming this is your method to check login status
+        builder: (context, snapshot) {
+          // Check if the future has completed
+          if (snapshot.connectionState == ConnectionState.done) {
+            // If we can determine the login status
+            if (snapshot.hasData && snapshot.data == true) {
+              return const HomeScreen(); // Or wherever you want to route after login
+            } else {
+              return const StartScreen(); // Your login or start screen
+            }
           } else {
-            return const StartScreen(); // Your login or start screen
+            return Scaffold(
+              backgroundColor: Theme.of(context).colorScheme.onPrimary, // Set the background color to white
+              body: Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min, // Use min to shrink the column's size to fit its children
+                  children: [
+                    // Wrap the CircularProgressIndicator in a SizedBox to increase its size
+                    SizedBox(
+                      width: 60, // Specify the size of the CircularProgressIndicator
+                      height: 60,
+                      child: CircularProgressIndicator(
+                        strokeWidth: 7, // Increase the stroke width to make the indicator bolder
+                        valueColor: AlwaysStoppedAnimation<Color>(Theme.of(context).colorScheme.onPrimaryContainer),
+                      ),
+                    ),
+                    const SizedBox(height: 20), // Add some spacing between the indicator and the text
+                    Text('Loading...', style: Theme.of(context).textTheme.titleLarge?.copyWith(color: Theme.of(context).colorScheme.onPrimaryContainer)),
+                  ],
+                ),
+              ),
+            );
+
           }
-        } else {
-          // Show a loading spinner while checking login status
-          return const Center(child: CircularProgressIndicator());
-        }
-      },
-    ),
-  );
-}
+        },
+      ),
+    );
+  }
 }
